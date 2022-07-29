298d63d1d7e4317b4734fc6a1c9ab2a29b3125e2
Samyr Christian Yentema Thiombiano <>
2022-07-29 01:14:44 +0000

    Add Boostrap and Boostrao-Vue

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
